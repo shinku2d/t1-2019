860db72c4782e58f63465c385bf491075da88fda
Ruben Mora Vargas <rmoravargas@hotmail.com>
2019-03-03 22:56:30 -0600

    Se crea una aplicación angular, se le agrega las librerias jquery, popper.js y bootstrap, se crean dos componentes nav-bar y footer  y se mejora la presentación, se cambia el favicon y el título de la página

---
+++ ce-web/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DatePipe } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [ DatePipe],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
